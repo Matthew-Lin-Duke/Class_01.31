ab76ad0607498a64b2678e0368bca273b683d34a
Jiacheng Lin <jl915@duke.edu>
2020-01-31 14:11:33 -0500

    Exercise additional work finished

--- test_y_coordinate.py
+++ test_y_coordinate.py
@@ -6,3 +6,25 @@ def test_cal_y():
     answer = cal_y(p1, p2, x3)
     expected = 26.8
     assert answer == expected
+
+
+def test_on_line_true():
+    from y_coordinate import on_line
+    p1 = (9, 10)
+    p2 = (4, 6)
+    p3 = (2, 4)
+    p4 = (30, 26.8)
+    answer = on_line(p1, p2, p4)
+    expected = True
+    assert answer == expected
+
+
+def test_on_line_false():
+    from y_coordinate import on_line
+    p1 = (9, 10)
+    p2 = (4, 6)
+    p3 = (2, 4)
+    p4 = (30, 26.8)
+    answer = on_line(p1, p2, p3)
+    expected = False
+    assert answer == expected
--- y_coordinate.py
+++ y_coordinate.py
@@ -8,10 +8,27 @@ def cal_y(p1,p2,x3):
     y3 = ((x3 - p1[0])/(p1[0]-p2[0]))*(p1[1]-p2[1])+ p1[1]
     return y3
 
+
+def on_line(p1, p2, p3):
+    flag = False
+    k = (p1[1]-p2[1])/(p1[0]-p2[0])
+    temp_y3 = (p3[0] - p1[0])*k + p1[1]
+    print(temp_y3)
+    if temp_y3 == p3[1]:
+        flag = True
+    return flag
+    
+    
+
 if __name__ == "__main__":
     #line_input()
     p1 = (9,10)
     p2 = (4,6)
+    p3 = (2,4)
+    p4 = (30, 26.8)
     x3 = 30
     y3 = cal_y(p1,p2,x3)
     print(y3)
+    flag_1 = on_line(p1, p2, p4)
+    print(flag_1)
+
